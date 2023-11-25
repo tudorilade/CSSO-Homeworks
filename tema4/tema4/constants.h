5b5e9cbf231ad8a123d81468552b92563a44ac73
tudorilade <85745605+tudorilade@users.noreply.github.com>
2023-11-25 17:24:04 +0000

    adding constants

---
+++ tema4/tema4/constants.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define CRITICAL_SECTION_MUTEX "criticalSectionMutex"
+#define DONATION_EVENT "donationEvent"
+#define SOLD_EVENT "soldEvent"
+#define DEPOSIT_EVENT "depoEvent"
+#define FINISHED_DONATION_EVENT "finishedDonationEvent"
+#define FINISHED_SOLD_EVENT "finishedSoldEvent"
+#define FINISHED_DEPOSIT_EVENT "finishedDepoEvent"
+
+#define MASTER_EVENT "masterEvent"
+#define TIME_TABLE_EVENT "timeTableEvent"
